f28ebdaab7cdb63e20afc88b619efef5d10fc9fc
jbosso8565 <jbosso8565@CHHB2ZKDXV1.inst.hcpss.org>
2018-04-11 14:01:54 -0400

    asfd

---
+++ Front End/AnimalFarm.java
@@ -0,0 +1,27 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+public class AnimalFarm
+{
+	public AnimalFarm() {
+		animalfarm  = new JFrame();
+		animalfarm.setTitle("animalfarm");
+		animalfarm.add(new MainMenu());
+		animalfarm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		animalfarm.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		AnimalFarm.animalfarm.dispose();
+		AnimalFarm.animalfarm.setUndecorated(true);
+		animalfarm.setVisible(true);
+	}
+	static Timer t = new Timer();
+	static JFrame animalfarm;
+	public static void main(String[] args){
+		new AnimalFarm();
+		t.schedule(new TimerTask(){
+			public void run() {
+				animalfarm.repaint();
+			}
+		}, 0,10);
+	}
+}
